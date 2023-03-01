63378f89a7e9b7222aa9f2e3c14ea65506046ade
Ruben Concha <ruben_shell@hotmail.com>
2023-02-28 23:00:24 -0300

    Primer Entrega Proyecto Integrador 28/02/2023

---
+++ cv.js
@@ -0,0 +1,46 @@
+
+
+
+/* ################################################################# */
+document.getElementById('MEstudio').onmouseenter = function () {
+    
+    document.getElementById('Div_Estudios').style.backgroundColor = "rgb(195, 245, 185)";
+}
+ 
+document.getElementById('MEstudio').onmouseleave = function () {
+    
+    document.getElementById('Div_Estudios').style.backgroundColor = "rgb(222, 248, 217)";
+}
+
+/* ################################################################# */
+document.getElementById('MExpeLab').onmouseenter = function () {
+    
+    document.getElementById('Div_Exp_Laboral').style.backgroundColor = "rgb(195, 245, 185)";
+}
+ 
+document.getElementById('MExpeLab').onmouseleave = function () {
+    
+    document.getElementById('Div_Exp_Laboral').style.backgroundColor = "rgb(222, 248, 217)";
+}
+
+/* ################################################################# */
+document.getElementById('MDescrPer').onmouseenter = function () {
+    
+    document.getElementById('Div_DescPersonal').style.backgroundColor = "rgb(195, 245, 185";
+}
+ 
+document.getElementById('MDescrPer').onmouseleave = function () {
+    
+    document.getElementById('Div_DescPersonal').style.backgroundColor = "rgb(222, 248, 217)";
+}
+
+/* ################################################################# */
+document.getElementById('MContactoR').onmouseenter = function () {
+    
+    document.getElementById('Div_Referencias').style.backgroundColor = "rgb(195, 245, 185";
+}
+ 
+document.getElementById('MContactoR').onmouseleave = function () {
+    
+    document.getElementById('Div_Referencias').style.backgroundColor = "rgb(222, 248, 217)";
+}
